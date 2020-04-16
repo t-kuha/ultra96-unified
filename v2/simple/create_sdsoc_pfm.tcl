65da725475933cad11dcb70c123de58ce4a7ee44
t-kuha <imagingtechnerd@gmail.com>
2020-04-16 22:55:46 +0900

    Add simple SDSoC platform source for v2 board

---
+++ v2/simple/create_sdsoc_pfm.tcl
@@ -0,0 +1,66 @@
+# May have to delete existing platform directory
+
+# Platform name
+set PFM_NAME    u96_sdx
+set SRC_DIR     pfm_files
+set PREBUILT_DATA_DIR   ${SRC_DIR}/prebuilt
+
+# Check whether prebuilt data folder exists
+set HAS_PREBUILT [file exists ${PREBUILT_DATA_DIR}]
+
+if { ${HAS_PREBUILT} } then {
+    set OUTDIR  _pfm
+} else {
+    set OUTDIR  _pfm_0
+}
+
+
+# Create platform
+platform create -name ${PFM_NAME} -hw ${PFM_NAME}.dsa -out ${OUTDIR}
+# {platform}
+
+
+# ----- Standalone -----
+sysconfig create -name "standalone" -display-name "standalone" -desc "Standalone / MPSoC" 
+
+sysconfig config -boot {pfm_files/boot}
+sysconfig config -bif {pfm_files/sdsoc_standalone.bif}
+sysconfig config -readme {pfm_files/generic.readme}
+
+# Use APU core #0
+domain create -name {standalone} -os {standalone} -proc {psu_cortexa53_0} -display-name {standalone} -desc "Standalone" -runtime "C++"
+
+# Add prebuilt data
+if { ${HAS_PREBUILT} } then {
+    domain config -prebuilt-data ${PREBUILT_DATA_DIR}
+}
+
+# Generate linker script automatically
+::scw::set_lscript_autogen true
+
+platform write
+
+
+# ----- Linux -----
+# Create system configuration
+sysconfig create -name "linux" -display-name "linux" -desc "Linux / MPSoC" 
+
+sysconfig config -boot {pfm_files/boot}
+sysconfig config -bif {pfm_files/sdsoc_linux.bif}
+sysconfig config -readme {pfm_files/generic.readme}
+
+domain create -name {linux} -os {linux} -proc {psu_cortexa53} -display-name {linux} -desc "Linux" -runtime "C++"
+
+# Add prebuilt data
+if { ${HAS_PREBUILT} } then {
+    domain config -prebuilt-data ${PREBUILT_DATA_DIR}
+}
+
+::scw::set_linux_configured true
+
+domain config -image {pfm_files/image}
+platform write
+
+
+# Generate platform
+platform generate
---
+++ v2/simple/create_vivado_project.tcl
@@ -0,0 +1,33 @@
+# Setting
+set PRJ_DIR     _vivado
+set PRJ_NAME    u96v2_sdx
+set BD_NAME     ${PRJ_NAME}
+set SRC_DIR     src
+set NUM_JOBS    4
+
+
+# Create project
+create_project ${PRJ_NAME} ${PRJ_DIR} -part xczu3eg-sbva484-1-e
+set_property board_part em.avnet.com:ultra96v2:part0:1.0 [current_project]
+
+# Create block design
+source $SRC_DIR/bd.tcl
+
+# Set top-level source
+make_wrapper -files [get_files ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${BD_NAME}/${BD_NAME}.bd] -top
+add_files -norecurse ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${BD_NAME}/hdl/${BD_NAME}_wrapper.v
+set_property top ${BD_NAME}_wrapper [current_fileset]
+update_compile_order -fileset sources_1
+
+# Generate block design
+regenerate_bd_layout
+# validate_bd_design
+save_bd_design
+generate_target all [get_files  ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${BD_NAME}/${BD_NAME}.bd]
+
+# Create .dsa
+write_dsa ${PRJ_NAME}.dsa
+validate_dsa ${PRJ_NAME}.dsa
+
+# Finish - close project
+close_project
