ca5fa5d368890956bdfa9c965338efc3974498c7
pentaxmedical <pentaxmedical.com>
2020-06-19 14:54:47 +0900

    Add files for creating HW

---
+++ v1/vitis-ai/create_xsa.tcl
@@ -0,0 +1,57 @@
+# Setting
+set PRJ_DIR     _vivado
+set PRJ_NAME    ultra96
+set BD_NAME     ${PRJ_NAME}
+set SRC_DIR     src
+set NUM_JOBS    2
+
+
+# Remove existing directory
+file delete -force ${PRJ_DIR}
+
+# Create project
+create_project ${PRJ_NAME} ${PRJ_DIR} -part xczu3eg-sbva484-1-e
+set_property board_part em.avnet.com:ultra96:part0:1.2 [current_project]
+
+# Create block design
+source ${SRC_DIR}/bd.tcl
+
+set_property platform.name "u96_accel" [current_project]
+set_property platform.design_intent.embedded "true" [current_project]
+set_property platform.design_intent.server_managed "false" [current_project]
+set_property platform.design_intent.external_host "false" [current_project]
+set_property platform.design_intent.datacenter "false" [current_project]
+set_property platform.default_output_type "sd_card" [current_project]
+
+# Set top-level source
+make_wrapper -files [get_files ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${BD_NAME}/${BD_NAME}.bd] -top
+add_files -norecurse ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${BD_NAME}/hdl/${BD_NAME}_wrapper.v
+set_property top ${BD_NAME}_wrapper [current_fileset]
+update_compile_order -fileset sources_1
+
+# Generate block design
+regenerate_bd_layout
+validate_bd_design
+save_bd_design
+generate_target all [get_files ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${BD_NAME}/${BD_NAME}.bd]
+
+# Generate bitstream
+update_compile_order -fileset sources_1
+
+reset_run synth_1
+launch_runs synth_1 -jobs ${NUM_JOBS}
+wait_on_run synth_1
+launch_runs impl_1 -to_step write_bitstream -jobs ${NUM_JOBS}
+wait_on_run impl_1
+
+# Report utilization & clock after implementation
+open_run impl_1
+report_utilization
+report_clocks
+
+# Export .xsa file
+write_hw_platform -force -include_bit -file ${PRJ_NAME}.xsa
+validate_hw_platform ${PRJ_NAME}.xsa
+
+# Finish - close project
+close_project
