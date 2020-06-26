8e7ff0c262b999e26924b893d269443a8cca15ed
t_kuha <imagingtechnerd@gmail.com>
2020-06-26 09:38:49 +0900

    Remove unnecessary comments

--- v1/embedded/create_app.tcl
+++ v1/embedded/create_app.tcl
@@ -14,7 +14,6 @@ platform write
 
 
 # Change stdin & stdout setting
-# standalone_domian
 domain active standalone_domain
 bsp config stdin psu_uart_1
 bsp config stdout psu_uart_1
@@ -35,8 +34,7 @@ platform generate
 # Creat e Hello World application (.elf)
 setws _application
 app create -name hello_world -hw ultra96.xsa -os standalone -proc psu_cortexa53_0 -template {Hello World}
-# app create -name test -sysproj ultra96 -domain standalone_domain -template {Hello World}
-# app create -name hello_world -platform ultra96 -domain standalone_domain -template {Hello World}
+
 # Build .elf in release mode
 app config -name hello_world -set build-config release
 app build -name hello_world
