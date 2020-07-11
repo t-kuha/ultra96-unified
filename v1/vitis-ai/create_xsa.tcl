f1867aadccbf67983c34f883bd0d597494f5be5d
kuriharat <pentaxmedical@vitis-2020.1-vm>
2020-07-11 18:54:58 +0900

    Update create_xsa.tcl (Do not run synthesis when creating platform)

--- v1/vitis-ai/create_xsa.tcl
+++ v1/vitis-ai/create_xsa.tcl
@@ -3,7 +3,6 @@ set PRJ_DIR     _vivado
 set PRJ_NAME    ultra96
 set BD_NAME     ${PRJ_NAME}
 set SRC_DIR     src
-set NUM_JOBS    2
 
 
 # Remove existing directory
@@ -35,22 +34,8 @@ validate_bd_design
 save_bd_design
 generate_target all [get_files ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${BD_NAME}/${BD_NAME}.bd]
 
-# Generate bitstream
-update_compile_order -fileset sources_1
-
-reset_run synth_1
-launch_runs synth_1 -jobs ${NUM_JOBS}
-wait_on_run synth_1
-launch_runs impl_1 -to_step write_bitstream -jobs ${NUM_JOBS}
-wait_on_run impl_1
-
-# Report utilization & clock after implementation
-open_run impl_1
-report_utilization
-report_clocks
-
 # Export .xsa file
-write_hw_platform -force -include_bit -file ${PRJ_NAME}.xsa
+write_hw_platform -force -file ${PRJ_NAME}.xsa
 validate_hw_platform ${PRJ_NAME}.xsa
 
 # Finish - close project
