5f436af6c4f9d9d51bb0d6af91d65fc822098532
t-kuha <imagingtechnerd@gmail.com>
2020-12-27 17:12:14 +0900

    Add files for creating HW

---
+++ v2/accel/create_xsa.tcl
@@ -0,0 +1,46 @@
+# Setting
+set PRJ_DIR     _vivado
+set PRJ_NAME    ultra96v2
+set BD_NAME     ${PRJ_NAME}
+set SRC_DIR     src
+set NUM_JOBS    4
+
+
+# Remove existing directory
+file delete -force ${PRJ_DIR}
+
+# Create project
+create_project ${PRJ_NAME} ${PRJ_DIR} -part xczu3eg-sbva484-1-e
+set_property board_part em.avnet.com:ultra96v2:part0:1.0 [current_project]
+
+set_property platform.name "u96v2_accel" [current_project]
+set_property platform.design_intent.embedded "true" [current_project]
+set_property platform.design_intent.server_managed "false" [current_project]
+set_property platform.design_intent.external_host "false" [current_project]
+set_property platform.design_intent.datacenter "false" [current_project]
+set_property platform.default_output_type "sd_card" [current_project]
+
+# Create block design
+source ${SRC_DIR}/bd.tcl
+
+set_property platform.vendor {avnet.com} [current_project]
+set_property platform.version {2020.2} [current_project]
+
+# Set top-level source
+make_wrapper -files [get_files [current_bd_design].bd] -top
+add_files -norecurse ${PRJ_DIR}/${PRJ_NAME}.srcs/sources_1/bd/${BD_NAME}/hdl/${BD_NAME}_wrapper.v
+set_property top ${BD_NAME}_wrapper [current_fileset]
+update_compile_order -fileset sources_1
+
+# Generate block design
+regenerate_bd_layout
+validate_bd_design
+save_bd_design
+generate_target all [get_files [current_bd_design].bd]
+
+# Export .xsa file
+write_hw_platform -force -file ${PRJ_NAME}.xsa
+validate_hw_platform ${PRJ_NAME}.xsa
+
+# Finish - close project
+close_project
