3f05442de935896fcd96eacdce862a15bf31f250
t-kuha <imagingtechnerd@gmail.com>
2023-11-18 21:36:39 +0900

    Reduce file copy when creating platform

--- v2/accel/create_vitis_platform.py
+++ v2/accel/create_vitis_platform.py
@@ -6,31 +6,18 @@ import vitis
 
 PFM_NAME = 'u96v2_accel'
 top_dir = os.path.dirname(__file__)
-top_dir = '.'
 pfm_dir = os.path.join(top_dir, '_pfm')
-boot_dir = os.path.join(top_dir, '_boot')
+# boot_dir = os.path.join(top_dir, '_boot')
 sd_dir = os.path.join(top_dir, '_sd_dir')
 petalinux_dir = os.path.join(top_dir, 'petalinux', 'images', 'linux')
 hw_xsa_path = os.path.join(top_dir, 'hw.xsa')
 hw_emu_xsa_path = os.path.join(top_dir, 'hw_emu.xsa')
 
 # prepare required files
-for d in [pfm_dir, boot_dir, sd_dir]:
+for d in [pfm_dir, sd_dir]:
     if os.path.exists(d):
         shutil.rmtree(d)
     os.mkdir(d)
-
-for fn in ['bl31.elf', 'pmufw.elf', 'system.dtb', ]:
-    shutil.copy(os.path.join(petalinux_dir, fn), boot_dir)
-
-shutil.copy(
-    os.path.join(petalinux_dir, 'u-boot-dtb.elf'),
-    os.path.join(boot_dir, 'u-boot.elf')
-)
-shutil.copy(
-    os.path.join(petalinux_dir, 'zynqmp_fsbl.elf'),
-    os.path.join(boot_dir, 'fsbl.elf')
-)
 
 for fn in ['boot.scr', 'image.ub']:
     shutil.copy(os.path.join(petalinux_dir, fn), sd_dir)
@@ -48,9 +35,10 @@ platform = client.create_platform_component(
 domain = platform.get_domain(name='linux_psu_cortexa53')
 status = domain.update_name(new_name='xrt')
 assert status
-status = domain.generate_bif()
+status = domain.add_bif(os.path.join(top_dir, 'src', 'linux.bif'))
+# status = domain.generate_bif()
 assert status
-status = domain.add_boot_dir(boot_dir)
+status = domain.add_boot_dir(petalinux_dir)
 assert status
 status = domain.set_sd_dir(sd_dir)
 assert status
