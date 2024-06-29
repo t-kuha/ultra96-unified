a18a001fa2978fab5ad0e5d8f730ae6a25d84db3
t-kuha <imagingtechnerd@gmail.com>
2024-06-29 10:55:13 +0000

    Add Python script for building sample app

---
+++ v2/accel/create_vadd_app.py
@@ -0,0 +1,44 @@
+"""
+Create vector addition app.
+
+How to build:
+    $ IDE_ZYNQMP_SYSROOT=<path to sysroot> vitis -s create_vadd_app.py
+"""
+import os
+import shutil
+
+import vitis
+
+# settings
+top_dir = os.path.dirname(os.path.abspath(__file__))
+ws_dir = os.path.join(top_dir, '_vitis-ws')
+pfm_dir = os.path.join(top_dir, '_pfm', 'u96v2_accel', 'export', 'u96v2_accel')
+
+# check input
+assert os.path.exists(pfm_dir)
+
+if os.path.exists(ws_dir):
+    print('[INFO] removing existing workspace...')
+    shutil.rmtree(ws_dir) 
+
+# start building
+client = vitis.create_client()
+
+status = client.set_workspace(path=ws_dir)
+assert (status)
+status = client.add_platform_repos(platform=pfm_dir)
+assert (status)
+proj = client.create_sys_project(
+    name='vadd',
+    platform=os.path.join(pfm_dir, 'u96v2_accel.xpfm'),
+    template='installed_examples/vadd'
+)
+# add "--package.no_image" option
+with open(os.path.join(ws_dir, 'vadd', 'package', 'package.cfg'), 'a') as file:
+    file.write('\n')
+    file.write('no_image=true')
+
+# proj = client.get_sys_project(name='vadd')
+proj.build(target='hw')
+
+vitis.dispose()
